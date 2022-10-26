89bf9449bab81fe7ec3977cf159db77c7e250a18
AmaniCoulibaly <a.coulibaly@alustudent.com>
2022-10-26 07:37:17 -0700

    fuctions 3

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    if i == 101 or i == 113:  
-    continue
-    else:
-    print("{}".format(chr(i)), end='')
+for c in range(ord('a'), ord('z')+1):
+     if c is not (ord('q')) and c is not (ord('e')):
+        print('{}'.format(chr(c)), end='')
