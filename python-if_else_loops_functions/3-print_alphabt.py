6bd19d6933fe8354b590de3b8880b0ed24fd22e7
AmaniCoulibaly <a.coulibaly@alustudent.com>
2022-10-26 07:29:00 -0700

    fuctions 3

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,8 +1,6 @@
-#!/usr/bin/python3
-for i in range(97, 123):
+#!/usr/bin/ython3
+for i in range(97, 123)
     if i == 101 or i == 113:  
-   
     continue
-    
-else:
+    else:
     print("{}".format(chr(i)), end='')
