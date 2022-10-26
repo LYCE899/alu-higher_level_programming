441bde50ae9ac7c9fa6fac7b3dd6409ed4f02315
AmaniCoulibaly <a.coulibaly@alustudent.com>
2022-10-26 08:45:20 -0700

    functions 7-islower second

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
-islower = __import__("7-islower").islower
-
-print("a is {}".format("lower" if islower("a") else "upper"))
-
-print("H is {}".format("lower" if islower("H") else "upper"))
-
-print("A is {}".format("lower" if islower("A") else "upper"))
-
-print("3 is {}".format("lower" if islower("3") else "upper"))
-
-print("g is {}".format("lower" if islower("g") else "upper"))
+def islower(c):
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False
