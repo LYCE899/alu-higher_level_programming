3d854f1c2e27eea17bc58eff32c4e847ce770d6c
AmaniCoulibaly <a.coulibaly@alustudent.com>
2022-11-03 13:18:45 -0700

    pthn data 0

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-print_list_integer = __import__('0-print_list_integer').print_list_integer
-
-my_list = [1, 2, 3,4,5]
-print('{:d}'.format(my_list))
+def print_list_integer(my_list=[]):
+    for item in my_list:
+        print('{:d}'.format(item))
