6970571b221b23d82feab35c3bbfca1f3252dab8
AmaniCoulibaly <a.coulibaly@alustudent.com>
2022-11-04 11:45:21 -0700

    pythn more data 12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,17 +1,35 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is None or type(roman_string) is not str:
+    if type(roman_string) != str or roman_string is None:
         return 0
-    if roman_string == "":
-        return 0
-    num = 0
-    dic = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-    for i, j in zip(roman_string, roman_string[1:]):
-        if i not in dic.keys():
-            return 0
-        elif dic[i] >= dic[j]:
-    num += dic[i]
-        else:
-            num -= dic[i]
-    num += dic[roman_string[-1]]
-    return num
+    roman_numerals = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+    }
+    sum_ = 0
+    sum_list = list()
+    if len(roman_string) == 1:
+       return roman_numerals[roman_string]
+    if len(roman_string) > 1:
+       for i in range(0, len(roman_string)):
+           try:
+               if roman_string[i] == "I" and \
+                       (roman_string[i + 1] == "V" or
+                       roman_string[i + 1] == "X") \
+                       or roman_string[i] == "X" and \
+                       (roman_string[i + 1] == "C" or
+                       roman_string[i + 1] == "L") \
+                       or roman_string[i] == "C" and \
+                       (roman_string[i + 1] == "D" or 
+                       roman_string[i + 1] == "M"):                                                                                                     sum_list.append(-int(roman_numerals[roman_string[i]]))
+                   else:
+                       sum_list.append(int(roman_numerals[roman_string[i]]))
+               except IndexError:
+                   sum_list.append(int(roman_numerals[roman_string[i]]))
+                   pass
+    return sum(sum_list)
