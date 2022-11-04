311ea249349cdc11bac6bf176c46e0bae80b651a
AmaniCoulibaly <a.coulibaly@alustudent.com>
2022-11-04 08:53:50 -0700

    pythn more data 1

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = my_list.copy()
-    if not my_list:
-    return new_list
-    for i in range(len(my_list)):
-        if new_list[i] == search:
-            new_list[i] = replace
-
-    return new_list
+    if my_list is not None:
+        return([x if x != search else replace for x in my_list])
+    return Nonereturn([x if x != search else replace for x in my_list])
+    return None
