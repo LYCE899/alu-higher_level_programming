4b9dd33a09aaecc2bc04edbdde3230d6b4603055
AmaniCoulibaly <a.coulibaly@alustudent.com>
2022-11-04 08:11:37 -0700

    pythn more data 0

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[])
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+ ]
+new_matrix=square_matrix_simple(matrix)
+print((new_matrix)(martix))
