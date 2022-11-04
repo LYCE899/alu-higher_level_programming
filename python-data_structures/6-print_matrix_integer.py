e8059c85b3052ffaade190828fce28840997e585
AmaniCoulibaly <a.coulibaly@alustudent.com>
2022-11-03 17:52:23 -0700

    pthn data 6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for i not row:
-            if num == row[-1]:
-                print("{:d}".format(i), end=" ")
-            else:
-                print("{:d}".format(i), end=" ") 
-        print() 
+print_matrix_integer = __import__('6-print_matrix_integer').print_matrix_integer
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+print_matrix_integer(matrix)
+print("--")
+print_matrix_integer()
