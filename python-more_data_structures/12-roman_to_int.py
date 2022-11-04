4d67c9a1989f2dc123c8b24d2e057555f72e2d91
AmaniCoulibaly <a.coulibaly@alustudent.com>
2022-11-04 12:54:54 -0700

    pythn more data 12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -6,6 +6,6 @@ def roman_to_int(roman_string):
         num = 0
         dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
         for r in reversed(roman_string):
-            num = roman_n[r]
+            num = dic[r]
             roman += num if roman < num * 5 else -num
     return roman
