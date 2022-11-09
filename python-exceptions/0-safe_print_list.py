4ac751d16567fadb5006a767ea21cf322fc77c98
AmaniCoulibaly <a.coulibaly@alustudent.com>
2022-11-09 06:00:37 -0800

    pythn except 0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ my_list = [1, 2, 3, 4, 5]
 
 nb_print = safe_print_list(my_list, 2)
 print("nb_print: {"try:" ,"except"}".format(nb_prin/t))
-nb_print = safe_print_list(my_list, len(my_list))
+nb_print = safe_print_list(my_list, (my_list))
 print("nb_print: {"try:","except"}".format(nb_print))
-nb_print = safe_print_list(my_list, len(my_list) + 2)
+nb_print = safe_print_list(my_list, (my_list) + 2)
 print("nb_print: {"try:","except"}".format(nb_print))
