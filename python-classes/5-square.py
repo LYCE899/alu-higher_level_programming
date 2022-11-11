a8a4d50283a81374a9c96983876e15f1f098bcd5
AmaniCoulibaly <a.coulibaly@alustudent.com>
2022-11-11 10:27:57 -0800

    pythn class 5

---
+++ python-classes/5-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+'''Creating a class'''
+
+
+class Square:
+    '''Square class with an attribute'''
+    def __init__(self, size=0):
+        '''Instance attribute'''
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = valueg
+    def area(self):
+        '''Returns the area of a square'''
+        return self.__size * self.__size
+
+    def my_print(self):                                                                                                                       if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
