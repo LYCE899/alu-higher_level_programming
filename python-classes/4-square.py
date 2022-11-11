15b65a5cf94a946aaedee9cf787edb66a9a5f5a1
AmaniCoulibaly <a.coulibaly@alustudent.com>
2022-11-11 10:01:53 -0800

    pythn class 4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -19,6 +19,7 @@ class Square:
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
     def area(self):
         '''Returns the area of a square'''
         return self.__size * self.__size
