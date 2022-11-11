1f48e37b365ee40fc94263cceb59619a00f26a19
AmaniCoulibaly <a.coulibaly@alustudent.com>
2022-11-11 08:16:22 -0800

    pythn class 4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -6,6 +6,7 @@ class Square:
     def __init__(self, size=0):
         '''Instance attribute'''
         self.__size = size
+
     @property
     def size(self):
         return self.__size
@@ -13,10 +14,10 @@ class Square:
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
-        raise TypeError("size must be an integer")
-    elif value < 0:
-        raise ValueError("size must be >= 0")
-    self.__size = value
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
-    def area(self):i                                                                                                                          '''Returns the area of a square'''
+    def area(self):i                                                      '''Returns the area of a square'''
         return self.__size * self.__size
