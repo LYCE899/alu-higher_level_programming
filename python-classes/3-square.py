2506185007069455eb3014e51122e50708c23f76
AmaniCoulibaly <a.coulibaly@alustudent.com>
2022-11-11 01:41:46 -0800

    pythn class 3

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python3
-"""Defines a class square"""
+'''Creating a class'''
 
 
 class Square:
-    """Represent a square."""
-
+    '''Square class with an attribute'''
     def __init__(self, size=0):
-        """Initialize a new square.
-        Args:
-        size (int): The size of the new square
-        """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
+        '''Instance attribute'''
         self.__size = size
 
+   @property
+   def size(self):
+       return self.__size
+
+   @size.setter
+   def size(self, value):
+       if not isinstance(value, int):
+           rais TypeError("size must be an integer")
+       elif value < 0:
+          raise ValueError("size must be >= 0")
+       self.__size = value
+
    def area(self):
-       """Return the current area of the square."""
-       return (self.__size * self.__size)
+       '''Returns the area of a square'''
+       return self.__size * self.__size
