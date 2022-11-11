d1a7a190fca5204789d350ebb9b585fe7afeadf1
AmaniCoulibaly <a.coulibaly@alustudent.com>
2022-11-11 07:26:05 -0800

    pythn class 3

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -15,6 +15,7 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
     def area(self):
         """Return the current area of the square."""
         return (self.__size * self.__size)
