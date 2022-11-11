c934a78203c97f3dab973b125f2ad2d32892a5e3
AmaniCoulibaly <a.coulibaly@alustudent.com>
2022-11-11 10:51:23 -0800

    pythn class 5

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -18,7 +18,8 @@ class Square:
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = valueg
+        self.__size = value
+
     def area(self):
         '''Returns the area of a square'''
         return self.__size * self.__size
