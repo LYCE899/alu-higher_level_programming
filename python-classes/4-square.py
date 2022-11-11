c7e8b5775a70d81258df96db451b3ed5dd16cc09
AmaniCoulibaly <a.coulibaly@alustudent.com>
2022-11-11 02:45:19 -0800

    pythn class 4

---
+++ python-classes/4-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''Creating a class'''
+
+
+class Square:
+    '''Square class with an attribute'''
+    def __init__(self, size=0):
+        '''Instance attribute'''
+        self.__size = size
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+    if not isinstance(value, int):
+        raise TypeError("size must be an integer")
+    elif value < 0:
+        raise ValueError("size must be >= 0")
+    self.__size = value
+
+    def area(self):i                                                                                                                            '''Returns the area of a square'''
+        return self.__size * self.__size
