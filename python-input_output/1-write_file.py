f1b801dad0ef305e5d28a7253951de51d7e268c9
Coulibaly Amani <109763914+LYCE899@users.noreply.github.com>
2022-11-18 01:06:42 +0200

    Update 1-write_file.py

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,1 +1,9 @@
-sur
+#!/usr/bin/python3
+'''Function that writes a string and returns the number of characters'''
+
+
+def write_file(filename="", text=""):
+    '''Defining a function that writes a string
+    returning the number of characters'''
+    with open(filename, 'w', encoding="utf-8") as f:
+        return f.write(text)
