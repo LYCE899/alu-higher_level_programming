e1d9ea32495ab29675477be66af53a8d6cdab3ba
AmaniCoulibaly <a.coulibaly@alustudent.com>
2022-11-17 11:39:14 -0800

    python best vertion 1

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,19 @@
+ class MyList that inherits from list'''
+
+
+
+
+
+ class MyList(list):
+
+     '''Creating a superclass list'''
+
+
+
+         def print_sorted(self):
+
+                 new_list = self[:]
+
+                         new_list.sort()
+
+                                 print("{}".format(new_list))
