f0d48f896e6c67e01dc54a86b6c82d0dc81d14d7
Coulibaly Amani <109763914+LYCE899@users.noreply.github.com>
2022-11-17 12:38:06 +0200

    Update 6-base_geometry.py

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -1,1 +1,10 @@
-my live 
+#!/usr/bin/python3
+'''Creating class BaseGeometry'''
+
+
+class BaseGeometry:
+    '''Writing class BaseGeometry'''
+
+    def area(self):
+        '''Function that raises an exception with a message'''
+        raise Exception("area() is not implemented")
