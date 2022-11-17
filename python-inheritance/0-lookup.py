30c9ed80123b67b2a0b089c5616bfce88ae80823
Coulibaly Amani <109763914+LYCE899@users.noreply.github.com>
2022-11-17 05:47:36 +0200

    Update 0-lookup.py

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,1 +1,10 @@
-urs
+#!/usr/bin/python3
+"""
+Module 0-lookup
+Contains method lookup that returns list of object's attribute and methods
+"""
+
+
+def lookup(obj):
+    """returns list of object's attribute and methods"""
+    return dir(obj)
