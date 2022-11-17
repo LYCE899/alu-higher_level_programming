28341ee251d4ec39cf144907dcabe04028d2845d
Coulibaly Amani <109763914+LYCE899@users.noreply.github.com>
2022-11-17 12:15:44 +0200

    Update 3-is_kind_of_class.py

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,1 +1,10 @@
-usd
+#!/usr/bin/python3
+'''Writing a function that checks
+if an object is an instance of a class or not
+'''
+
+
+def is_kind_of_class(obj, a_class):
+    '''A function that checks if an object is an
+    '''
+    return isinstance(obj, a_class)
