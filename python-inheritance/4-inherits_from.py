2d69ad162b031885ae51249c82d749dbfcb70b77
Coulibaly Amani <109763914+LYCE899@users.noreply.github.com>
2022-11-17 12:26:14 +0200

    Update 4-inherits_from.py

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,1 +1,8 @@
-vwimy live
+#!/usr/bin/python3
+'''Writing a function that checks if an object is
+an instance of a class that is inherited directly or indirectly'''
+
+
+def inherits_from(obj, a_class):
+    '''Direct and indirect inheritance'''
+    return isinstance(obj, a_class) and type(obj) != a_class
