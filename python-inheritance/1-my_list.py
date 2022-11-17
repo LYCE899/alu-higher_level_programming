e28528ce8dda6c885a1772a52ac177e38982bde2
Coulibaly Amani <109763914+LYCE899@users.noreply.github.com>
2022-11-17 21:47:50 +0200

    Update 1-my_list.py

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,19 +1,11 @@
- class MyList that inherits from list'''
+#!/usr/bin/python3
+'''Writing a class MyList that inherits from list'''
 
 
+class MyList(list):
+    '''Creating a superclass list'''
 
-
-
- class MyList(list):
-
-     '''Creating a superclass list'''
-
-
-
-         def print_sorted(self):
-
-                 new_list = self[:]
-
-                         new_list.sort()
-
-                                 print("{}".format(new_list))
+    def print_sorted(self):
+        new_list = self[:]
+        new_list.sort()
+        print("{}".format(new_list))
