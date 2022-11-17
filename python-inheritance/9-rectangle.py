cb0e76fc7cce94694c7b9648656879d5db274349
Coulibaly Amani <109763914+LYCE899@users.noreply.github.com>
2022-11-18 00:30:24 +0200

    Update 9-rectangle.py

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -6,35 +6,19 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-
     '''Creating a class that inherits properties from BaseGeometry'''
 
-
-
     def __init__(self, width, height):
-
         '''Function that takes in width and height'''
-
         self.integer_validator('width', width)
-
         self.__width = width
-
         self.integer_validator('height', height)
-
         self.__height = height
 
-
-
     def area(self):
-
         '''Defining an area function'''
-
         return self.__height * self.__width
 
-
-
     def __str__(self):
-
         '''Returns [Rectangle] '''
-
         return str("[Rectangle] {}/{}".format(self.__width, self.__height))
