ba5b8673e5e5fb711e39009082ca720e74bc3e92
AmaniCoulibaly <a.coulibaly@alustudent.com>
2022-11-17 13:02:14 -0800

    python best vertion 7

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+'''Creating class BaseGeometry'''
+
+
+
+
+
+class BaseGeometry:
+
+        '''Writing class BaseGeometry'''
+
+
+
+            def area(self):
+
+                        '''Function that raises an exception with a message'''
+
+                                raise Exception("area() is not implemented")
+
+
+
+                                def integer_validator(self, name, value):
+
+                                            '''Creating a function that validates the value'''
+
+                                                    if type(value) != int:
+
+                                                                    raise TypeError(f"{name} must be an integer")
+
+
+
+                                                                        if value <= 0:
+
+                                                                                        raise ValueError(f"{name} must be greater than 0")
