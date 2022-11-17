35c306b190586277438b7dd323dddc8f5ea41cb8
Coulibaly Amani <109763914+LYCE899@users.noreply.github.com>
2022-11-18 00:38:19 +0200

    Update 11-square.py

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,47 +1,24 @@
 #!/usr/bin/python3
-
 '''Class square which inherits from Rectangle'''
 
 
-
-
-
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
-
 Rectangle = __import__('9-rectangle').Rectangle
 
 
-
-
-
 class Square(Rectangle):
+    '''Creating a class square that inherits from Rectangle'''
 
-        '''Creating a class square that inherits from Rectangle'''
+    def __init__(self, size):
+        '''Defining the init function'''
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
 
+    def area(self):
+        '''Defining the area function'''
+        return self.__size ** 2
 
-
-            def __init__(self, size):
-
-                        '''Defining the init function'''
-
-                                self.integer_validator("size", size)
-
-                                        super().__init__(size, size)
-
-                                                self.__size = size
-
-
-
-                                                    def area(self):
-
-                                                                '''Defining the area function'''
-
-                                                                        return self.__size ** 2
-
-
-
-                                                                        def __str__(self):
-
-                                                                                    '''Returns [Square]'''
-
-                                                                                            return str("[Square] {}/{}".format(self.__size, self.__size))
+    def __str__(self):
+        '''Returns [Square]'''
+        return str("[Square] {}/{}".format(self.__size, self.__size))
