751a5028827c14998d426bbb744f2079e383b7c1
Coulibaly Amani <109763914+LYCE899@users.noreply.github.com>
2022-11-18 00:58:09 +0200

    Update 0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,1 +1,9 @@
-allah
+#!/usr/bin/python3
+'''Function that reads a text files and prints out its stdout'''
+
+
+def read_file(filename=""):
+    '''Defining a function that reads a text file'''
+    with open(filename, 'r', encoding="utf-8") as f:
+        read_data = f.read()
+        print(read_data, end="")
