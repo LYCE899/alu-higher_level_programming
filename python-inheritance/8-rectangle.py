5804dc5f72fe08e654db2d4735abda95b052550a
AmaniCoulibaly <a.coulibaly@alustudent.com>
2022-11-17 03:38:46 -0800

    pythn inherit 8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,16 +1,1 @@
-#!/usr/bin/python3
-'''Creating class Rectangle'''
-
-
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
-
-
-class Rectangle(BaseGeometry):
-    '''Creating a class that inherits properties from BaseGeometry'''
-
-    def __init__(self, width, height):
-        '''Function that takes in width and height'''
-        self.integer_validator('width', width)
-        self.__width = width
-        self.integer_validator('height', height)
-        self.__height = height
+my live 
