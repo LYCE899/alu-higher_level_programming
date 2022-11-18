37c9853d497c2cb1198e9bd4805ff78945c4c8d0
Coulibaly Amani <109763914+LYCE899@users.noreply.github.com>
2022-11-18 02:18:38 +0200

    Update 2-append_write.py

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,1 +1,8 @@
-ytr
+#!/usr/bin/python3
+'''Appending a string and returning the number of characters added'''
+
+
+def append_write(filename="", text=""):
+    '''Function that appends a string file'''
+    with open(filename, 'a', encoding="utf-8") as f:
+        return f.write(text)
