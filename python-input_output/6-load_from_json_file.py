a482074613418dc3c914c33c774160ff7a2fb4a8
Coulibaly Amani <109763914+LYCE899@users.noreply.github.com>
2022-11-18 02:35:18 +0200

    Update 6-load_from_json_file.py

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,1 +1,11 @@
-out
+#!/usr/bin/python3
+'''Creating an object from a JSON file'''
+
+
+import json
+
+
+def load_from_json_file(filename):
+    '''Function that creates an object from a JSON file'''
+    with open(filename, 'r', encoding="utf-8") as f:
+        return json.load(f)
