6c80cd0eb74ccecf34ce104f771cbd1cdb16d15b
Coulibaly Amani <109763914+LYCE899@users.noreply.github.com>
2022-11-18 02:28:52 +0200

    Update 4-from_json_string.py

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,1 +1,10 @@
-dle
+#!/usr/bin/python3
+'''Returning an object by a JSON string'''
+
+
+import json
+
+
+def from_json_string(my_str):
+    '''Function that returns an object represented by a JSON string'''
+    return json.loads(my_str)
