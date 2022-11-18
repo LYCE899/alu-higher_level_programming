724997043329d056ae72533d364177949ff99bba
Coulibaly Amani <109763914+LYCE899@users.noreply.github.com>
2022-11-18 02:54:32 +0200

    Update 10-student.py

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,1 +1,33 @@
-dre
+#!/usr/bin/python3
+'''Writing class student'''
+
+
+class Student:
+    '''Defining a class called student'''
+    first_name = None
+    last_name = None
+    age = None
+
+    def __init__(self, first_name, last_name, age):
+        '''Instantiation of data given'''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        '''returns a dict rep of the data'''
+        context = {
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'age': self.age
+        }
+        if attrs is None or type(attrs) != list:
+            return context
+        else:
+            cont = {}
+            for item in attrs:
+                if type(item) != str:
+                    return context
+                if item in context.keys():
+                    cont[item] = context[item]
+            return cont
