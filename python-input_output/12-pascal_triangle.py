e5d1f0ced37cfb87835f849752d4e57eca4213cf
Coulibaly Amani <109763914+LYCE899@users.noreply.github.com>
2022-11-18 03:04:36 +0200

    Update 12-pascal_triangle.py

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,1 +1,26 @@
-der
+#!/usr/bin/python3
+'''Representing the Pascal's triangle'''
+
+
+def pascal_triangle(n):
+    '''Creating a function that returns a list of lists'''
+    outer_list = []
+
+    for i in range(n):
+        if i == 0:
+            outer_list.append([1])
+        elif i == 1:
+            outer_list.append([1, 1])
+        else:
+            last_list = outer_list[-1]
+            count = 0
+            new_list = [1]
+            while count < len(last_list):
+                try:
+                    new_list.append(last_list[count] + last_list[count + 1])
+                    count += 1
+                except IndexError:
+                    break
+            new_list.append(1)
+            outer_list.append(new_list)
+    return outer_list
