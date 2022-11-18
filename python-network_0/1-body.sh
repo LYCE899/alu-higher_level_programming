7f4f9fc5991a7119911495670373803bdc2d45b3
Coulibaly Amani <109763914+LYCE899@users.noreply.github.com>
2022-11-18 16:55:19 +0200

    Update 1-body.sh

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#Script that takes in URL, sends a GET request to the URL
-curl -sLfG "$1"
+# Take in URL, display body of a 200 response; Usage: ./1-body.sh 5B0.0.0.0:5000/route_1 ; echo ""
+curl -sL "$1"
