76145a8a65b9bfb0d213583cc71ad111ef4b4072
Coulibaly Amani <109763914+LYCE899@users.noreply.github.com>
2022-11-18 02:50:27 +0200

    Update 9-student.py

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,1 +1,24 @@
-dfr
+#!/usr/bin/python3
+'''Writing class student'''
+
+
+class Student:
+    '''Defining a class called student'''
+    first_name = None
+    last_name = None
+    age = None
+
+    def __init__(self, first_name, last_name, age):
+        '''Instantiation of data given'''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        '''Retrieving a dictionary representation'''
+        context = {
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'age': self.age
+        }
+        return context
