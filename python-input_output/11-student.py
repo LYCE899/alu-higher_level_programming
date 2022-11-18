a92e87664be15b40bae640158894ca685277f5e6
Coulibaly Amani <109763914+LYCE899@users.noreply.github.com>
2022-11-18 02:59:23 +0200

    Update 11-student.py

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,1 +1,38 @@
-ret
+#!/usr/bin/python3
+'''Writing class student'''
+
+
+class Student:
+    '''Defining a class called student'''
+    first_name = None
+    last_name = None
+    age = None
+
+    def __init__(self, first_name, last_name, age):
+        '''Instantiation of data given'''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        '''Returning a dictionary representation'''
+        context = {
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'age': self.age
+        }
+        if attrs is None or type(attrs) != list:
+            return context
+        else:
+            cont = {}
+            for item in attrs:
+                if type(item) != str:
+                    return context
+                if item in context.keys():
+                    cont[item] = context[item]
+            return cont
+
+    def reload_from_json(self, json):
+        '''Reload_from_json'''
+        for item in json.keys():
+            self.__dict__[item] = json[item]
