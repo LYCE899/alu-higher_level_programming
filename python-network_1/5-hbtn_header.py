8dcee386a9396dfa4c09dc3d223913bd933d20b0
Coulibaly Amani <109763914+LYCE899@users.noreply.github.com>
2022-11-22 15:55:36 +0200

    Update 5-hbtn_header.py

--- python-network_1/5-hbtn_header.py
+++ python-network_1/5-hbtn_header.py
@@ -1,29 +1,15 @@
 #!/usr/bin/python3
-
 """
-
 Module 5-hbtn_header.py
-
 """
-
-
-
 
 
 from sys import argv
-
 import requests
 
 
-
-
-
 if __name__ == "__main__":
-
-        url = argv[1]
-
-            le = requests.get(url)
-
-                la = le.headers.get('X-Request-Id')
-
-                    print(la)
+    url = argv[1]
+    le = requests.get(url)
+    la = le.headers.get('X-Request-Id')
+    print(la)
