4e5ccec760d189ae2a668b16fc6b20223a6d4e25
Coulibaly Amani <109763914+LYCE899@users.noreply.github.com>
2022-11-22 15:58:50 +0200

    Update 6-post_email.py

--- python-network_1/6-post_email.py
+++ python-network_1/6-post_email.py
@@ -1,25 +1,13 @@
 #!/usr/bin/python3
-
 """
-
 Module 6-post_email.py
-
 """
-
-
-
 
 
 import requests
-
 from sys import argv
 
 
-
-
-
 if __name__ == "__main__":
-
-        resp = requests.post(argv[1], data={"email": argv[2]})
-
-            print(resp.text)
+    resp = requests.post(argv[1], data={"email": argv[2]})
+    print(resp.text)
