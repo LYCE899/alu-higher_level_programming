5fb79484e0e93efc098fc8e47a98c2d0e22a4133
Coulibaly Amani <109763914+LYCE899@users.noreply.github.com>
2022-11-22 15:37:10 +0200

    Update 1-hbtn_header.py

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,26 +1,15 @@
-
-dule 1-hbtn_header.py
-
+#!/usr/bin/python3
+"""
+Module 1-hbtn_header.py
 """
 
 
-
-
-
 import urllib.request
-
 from sys import argv
 
 
-
-
-
 if __name__ == "__main__":
-
-        req = urllib.request.Request(argv[1])
-
-            with urllib.request.urlopen(req) as response:
-
-                    data = response.getheader('X-Request-Id')
-
-                            print(data)
+    req = urllib.request.Request(argv[1])
+    with urllib.request.urlopen(req) as response:
+        data = response.getheader('X-Request-Id')
+        print(data)
