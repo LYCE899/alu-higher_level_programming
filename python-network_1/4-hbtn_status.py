8c3c6cdaa288a0647a72bf08d59e57988b30ba2a
Coulibaly Amani <109763914+LYCE899@users.noreply.github.com>
2022-11-22 15:48:50 +0200

    Update 4-hbtn_status.py

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,25 +1,13 @@
 #!/usr/bin/python3
-
 """
-
 Module 4-hbtn_status.py
-
 """
-
-
 
 import requests
 
 
-
-
-
 if __name__ == "__main__":
-
-        r = requests.get("https://intranet.hbtn.io/status")
-
-            print("Body response:")
-
-                print("\t- type: {}".format(type(r.text)))
-
-                    print("\t- content: {}".format(r.text))
+    r = requests.get("https://intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
