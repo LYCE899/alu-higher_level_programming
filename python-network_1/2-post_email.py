3dce0d8d4dc1ff39dbaa7122263ece4b58fdac0b
Coulibaly Amani <109763914+LYCE899@users.noreply.github.com>
2022-11-22 15:41:15 +0200

    Update 2-post_email.py

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,36 +1,20 @@
-
-dule 2-post_email.py
-
+#!/usr/bin/python3
+"""
+Module 2-post_email.py
 """
 
 
-
-
-
 import urllib.parse
-
 import urllib.request
-
 from sys import argv
 
 
-
-
-
 if __name__ == "__main__":
-
-        url = argv[1]
-
-            value = {"email": argv[2]}
-
-                data = urllib.parse.urlencode(value)
-
-                    data = data.encode('ascii')
-
-                        req = urllib.request.Request(url, data)
-
-                            with urllib.request.urlopen(req) as response:
-
-                                    page = response.read().decode('utf-8')
-
-                                            print(page)
+    url = argv[1]
+    value = {"email": argv[2]}
+    data = urllib.parse.urlencode(value)
+    data = data.encode('ascii')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        page = response.read().decode('utf-8')
+        print(page)
