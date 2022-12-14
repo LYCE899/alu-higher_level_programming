dfd246cc85fccd4630e39dae8c60815d395f0a91
Coulibaly Amani <109763914+LYCE899@users.noreply.github.com>
2022-12-14 17:55:42 +0200

    Update 1-filter_states.py

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""A script that lists all states with a name
+starting with N from the database"""
+
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(
+       user=sys.argv[1],
+       password=sys.argv[2],
+       db=sys.argv[3],
+       host="localhost",
+       port=3306
+    )
+
+    cur = conn.cursor()
+
+    cur.execute("SELECT * FROM states \
+        WHERE name LIKE BINARY 'N%' \
+        ORDER BY id ASC")
+
+    states = cur.fetchall()
+
+    for state in states:
+        print(state)
+
+    cur.close()
+    conn.close()
