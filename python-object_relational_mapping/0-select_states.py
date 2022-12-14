7d05938f490971561786b6e618bdbb52a7314303
Coulibaly Amani <109763914+LYCE899@users.noreply.github.com>
2022-12-14 17:54:56 +0200

    Update 0-select_states.py

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Writing a script that lists all the states from a database"""
+
+
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    db_conn = MySQLdb.connect(
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        host='localhost',
+        port=3306
+    )
+
+    cur = db_conn.cursor()
+
+    cur.execute(
+        "SELECT * FROM states ORDER BY id ASC")
+    states = cur.fetchall()
+
+    for state in states:
+        print(state)
+
+    cur.close()
+    db_conn.close()
