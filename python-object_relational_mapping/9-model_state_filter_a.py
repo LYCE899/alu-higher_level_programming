d87f848cf987686451a54a1ebe949641ed1eb0e7
Amani coulibaly <a.coulibaly@alustudent.com>
2022-12-15 06:31:55 -0800

    ORM 9

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,20 +1,53 @@
-#!/usr/bin/python3
-""" list objects"""
+cript that lists all State objects that
+
+contain the letter a from the database"""
+
+
+
+
+
+import sys
+
+from model_state import Base, State
+
+from sqlalchemy.orm import sessionmaker
+
+from sqlalchemy import create_engine
+
+
+
 
 
 if __name__ == "__main__":
-    from sys import argv
-    from model_state import State, Base
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import sessionmaker
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        argv[1], argv[2], argv[3]), pool_pre_ping=True)
-    session = sessionmaker(bind=engine)
-    Base.metadata.create_all(engine)
-    st = session().query(State).filter(State.name.like('%a%')).order_by(
-        State.id).all()
-    if st:
-        for stat in st:
-            if 'a' in stat.name:
-                print("{}: {}".format(stat.id, stat.name))
-    session().close()
+
+        engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+
+                                sys.argv[1], sys.argv[2], sys.argv[3]),
+
+                                                        pool_pre_ping=True
+
+                                                                            )
+
+                                                                                Session = sessionmaker(bind=engine)
+
+
+
+                                                                                    Base.metadata.create_all(engine)
+
+
+
+                                                                                        session = Session()
+
+
+
+                                                                                            states = session.query(State).filter(State.name.contains('a')).all()
+
+
+
+                                                                                                for state in states:
+
+                                                                                                        print(f'{state.id}: {state.name}')
+
+
+
+                                                                                                            session.close()
