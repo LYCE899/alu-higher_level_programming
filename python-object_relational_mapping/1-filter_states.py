660c9a4c832e797f3945d479c00f452adf348f7c
Coulibaly Amani <109763914+LYCE899@users.noreply.github.com>
2022-12-15 11:51:33 +0200

    Update 1-filter_states.py

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,20 +1,26 @@
 #!/usr/bin/python3
-""" module list states
-from database"""
+"""
+return states starting with 'N'
+parameters given to script: username, password, database
+"""
+
+import MySQLdb
+from sys import argv
 
 if __name__ == "__main__":
-    import MySQLdb
-    from sys import argv
-    # port and host are default local and 3306
-    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
-    cur = db.cursor()
-    cur.execute("""SELECT * FROM states ORDER BY states.id ASC""")
-    result = cur.fetchall()
-    # display elements with N
-    # only by taking comparing their first letter in tuple
-    for i in result:
-        if i[1][0] == 'N':
-            print(i)
-    # close cursor and db
-    cur.close()
+
+    # connect to database
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+
+    # create cursor to exec queries using SQL; filter names starting with 'N'
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    for row in cursor.fetchall():
+        if row[1][0] == 'N':
+            print(row)
+    cursor.close()
     db.close()
