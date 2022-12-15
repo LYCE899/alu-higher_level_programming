e9c57b9a8bb644691c84f34bf64bc0e0caa9b90e
Coulibaly Amani <109763914+LYCE899@users.noreply.github.com>
2022-12-15 16:20:04 +0200

    Update 8-model_state_fetch_first.py

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,49 +1,25 @@
 #!/usr/bin/python3
-
 """A script that prints the first State object from database"""
 
 
-
-
-
 import sys
-
 from sqlalchemy import create_engine
-
 from sqlalchemy.orm import Session
-
 from model_state import Base, State
 
-
-
 if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
 
-        engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+    session = Session(engine)
 
-                                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+    record = session.query(State).first()
 
-                                                                      pool_pre_ping=True)
+    if record:
+        print("{}: {}".format(record.__dict__['id'], record.__dict__['name']))
+    else:
+        print("Nothing")
 
-            Base.metadata.create_all(engine)
-
-
-
-                session = Session(engine)
-
-
-
-                    record = session.query(State).first()
-
-
-
-                        if record:
-
-                                    print("{}: {}".format(record.__dict__['id'], record.__dict__['name']))
-
-                                        else:
-
-                                                    print("Nothing")
-
-
-
-                                                        session.close()
+    session.close()
