de460df0248e544f11ddba8a3601b9e07689eb2e
Amani coulibaly <a.coulibaly@alustudent.com>
2022-12-15 06:19:19 -0800

    ORM 8

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,25 +1,49 @@
 #!/usr/bin/python3
+
 """A script that prints the first State object from database"""
 
 
+
+
+
 import sys
+
 from sqlalchemy import create_engine
+
 from sqlalchemy.orm import Session
+
 from model_state import Base, State
 
+
+
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-    Base.metadata.create_all(engine)
 
-    session = Session(engine)
+        engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
 
-    record = session.query(State).first()
+                                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
 
-    if record:
-        print("{}: {}".format(record.__dict__['id'], record.__dict__['name']))
-    else:
-        print("Nothing")
+                                                                      pool_pre_ping=True)
 
-    session.close()
+            Base.metadata.create_all(engine)
+
+
+
+                session = Session(engine)
+
+
+
+                    record = session.query(State).first()
+
+
+
+                        if record:
+
+                                    print("{}: {}".format(record.__dict__['id'], record.__dict__['name']))
+
+                                        else:
+
+                                                    print("Nothing")
+
+
+
+                                                        session.close()
