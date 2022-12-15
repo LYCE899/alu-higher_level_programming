96adb67a68a05d16f21403c90639f56636080288
Amani coulibaly <a.coulibaly@alustudent.com>
2022-12-15 06:55:46 -0800

    ORM 14

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,28 +1,55 @@
 #!/usr/bin/python3
+
 """Script that prints all City objects from database"""
 
 
+
+
+
 import sys
+
 from sqlalchemy import create_engine
+
 from sqlalchemy.orm import Session
+
 from model_state import Base, State
+
 from model_city import City
 
+
+
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-    Base.metadata.create_all(engine)
 
-    session = Session(engine)
+        engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
 
-    states = session.query(State, City) \
-        .filter(State.id == City.state_id).order_by(City.id).all()
+                                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
 
-    for state in states:
-        print("{}: ({}) {}".format(
-            state[0].__dict__['name'],
-            state[1].__dict__['id'],
-            state[1].__dict__['name']))
+                                                                      pool_pre_ping=True)
 
-    session.close()
+            Base.metadata.create_all(engine)
+
+
+
+                session = Session(engine)
+
+
+
+                    states = session.query(State, City) \
+
+                                    .filter(State.id == City.state_id).order_by(City.id).all()
+
+
+
+                                        for state in states:
+
+                                                    print("{}: ({}) {}".format(
+
+                                                                    state[0].__dict__['name'],
+
+                                                                                state[1].__dict__['id'],
+
+                                                                                            state[1].__dict__['name']))
+
+
+
+                                                        session.close()
