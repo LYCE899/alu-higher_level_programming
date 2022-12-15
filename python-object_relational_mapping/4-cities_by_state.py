31efbbb3a1a8c1f92e99af750de9db1ba00966c7
Coulibaly Amani <109763914+LYCE899@users.noreply.github.com>
2022-12-15 16:00:28 +0200

    Update 4-cities_by_state.py

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,61 +1,31 @@
 #!/usr/bin/python3
-
 """Script that lists all cities from a database"""
 
 
-
-
-
 import sys
-
 import MySQLdb
 
 
-
-
-
 if __name__ == "__main__":
+    conn = MySQLdb.connect(
+        user=sys.argv[1],
+        password=sys.argv[2],
+        db=sys.argv[3],
+        host="localhost",
+        port=3306
+    )
+    cursor = conn.cursor()
 
-        conn = MySQLdb.connect(
+    sql = """SELECT c.id, c.name, s.name
+          FROM states s, cities c
+          WHERE c.state_id = s.id
+          ORDER BY c.id ASC"""
 
-                        user=sys.argv[1],
+    cursor.execute(sql)
+    cities = cursor.fetchall()
 
-                                password=sys.argv[2],
+    for city in cities:
+        print(city)
 
-                                        db=sys.argv[3],
-
-                                                host="localhost",
-
-                                                        port=3306
-
-                                                            )
-
-            cursor = conn.cursor()
-
-
-
-                sql = """SELECT c.id, c.name, s.name
-
-                          FROM states s, cities c
-
-                                    WHERE c.state_id = s.id
-
-                                              ORDER BY c.id ASC"""
-
-
-
-                                                  cursor.execute(sql)
-
-                                                      cities = cursor.fetchall()
-
-
-
-                                                          for city in cities:
-
-                                                                      print(city)
-
-
-
-                                                                          cursor.close()
-
-                                                                              conn.close()
+    cursor.close()
+    conn.close()
