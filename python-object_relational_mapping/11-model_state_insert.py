fc8a00a43017383129eafb77afd871fa2f041b4b
Coulibaly Amani <109763914+LYCE899@users.noreply.github.com>
2022-12-15 16:43:46 +0200

    Update 11-model_state_insert.py

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -1,45 +1,23 @@
 #!/usr/bin/python3
-
 """A script that adds the State object “Louisiana” to database"""
 
 
-
-
-
 import sys
-
 from model_state import Base, State
-
 from sqlalchemy.orm import sessionmaker
-
 from sqlalchemy import create_engine
 
 
-
-
-
 if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    Base.metadata.create_all(engine)
 
-        engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-
-                                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-
-                                                                      pool_pre_ping=True)
-
-            Session = sessionmaker(bind=engine)
-
-                session = Session()
-
-                    Base.metadata.create_all(engine)
-
-
-
-                        state = State(name="Louisiana")
-
-                            session.add(state)
-
-                                session.commit()
-
-                                    print(state.id)
-
-                                        session.close()
+    state = State(name="Louisiana")
+    session.add(state)
+    session.commit()
+    print(state.id)
+    session.close()
