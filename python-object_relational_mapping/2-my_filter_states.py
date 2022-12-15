35119649e1f628cb1310a1365c3c762d1bf444b6
Amani coulibaly <a.coulibaly@alustudent.com>
2022-12-15 05:49:09 -0800

    ORM 2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,29 +1,31 @@
 #!/usr/bin/python3
-"""
-return matching states
-parameters given to script: username, password, database, state to match
-"""
 
-import MySQLdb
-from sys import argv
+"""list states from databased"""
+
+
+
+
 
 if __name__ == "__main__":
 
-    # connect to database
-    db = MySQLdb.connect(host="localhost",
-                         port=3306,
-                         user=argv[1],
-                         passwd=argv[2],
-                         db=argv[3])
+        import MySQLdb
 
-    # create cursor to exec queries using SQL; match arg given
-    cursor = db.cursor()
-    sql_cmd = """SELECT *
-                 FROM states
-                 WHERE name LIKE '{:s}' ORDER BY id ASC""".format(argv[4])
-    cursor.execute(sql_cmd)
-    for row in cursor.fetchall():
-        if row[1] == argv[4]:
-            print(row)
-    cursor.close()
-    db.close()
+            from sys import argv
+
+                db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+
+                    cur = db.cursor()
+
+                        cur.execute("SELECT * FROM states WHERE states.name = '{}' OREDER BY states.id ASC".format(argv[4]))
+
+                            result = cur.fetchall()
+
+                                for i in result:
+
+                                            if i[1] == argv[4]:
+
+                                                            print(i)
+
+                                                                cur.close()
+
+                                                                    db.close()
