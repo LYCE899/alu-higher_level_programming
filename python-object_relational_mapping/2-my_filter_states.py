2e784de7395ad875916722e87ab81f23dda83491
Coulibaly Amani <109763914+LYCE899@users.noreply.github.com>
2022-12-15 15:53:21 +0200

    Update 2-my_filter_states.py

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,16 +1,30 @@
 #!/usr/bin/python3
-"""list states from databased"""
+"""Script that takes in an argument and
+displays all values in the states"""
+
+
+import sys
+import MySQLdb
 
 
 if __name__ == "__main__":
-    import MySQLdb
-    from sys import argv
-    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE states.name = '{}' OREDER BY states.id ASC".format(argv[4]))
-    result = cur.fetchall()
-    for i in result:
-        if i[1] == argv[4]:
-            print(i)
-    cur.close()
-    db.close()
+    conn = MySQLdb.connect(
+        user=sys.argv[1],
+        password=sys.argv[2],
+        db=sys.argv[3],
+        host="localhost",
+        port=3306
+    )
+    cursor = conn.cursor()
+    sql = """ SELECT * FROM states
+        WHERE name LIKE BINARY '{}'
+        ORDER BY id ASC """.format(sys.argv[4])
+
+    cursor.execute(sql)
+    data = cursor.fetchall()
+
+    for row in data:
+        print(row)
+
+    cursor.close()
+    conn.close()
