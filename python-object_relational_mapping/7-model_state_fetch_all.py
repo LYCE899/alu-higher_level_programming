566626d663c3a8ee940505f665a8fed5d4d37eee
Coulibaly Amani <109763914+LYCE899@users.noreply.github.com>
2022-12-15 16:11:52 +0200

    Update 7-model_state_fetch_all.py

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,51 +1,26 @@
 #!/usr/bin/python3
-
 """Writing a script that lists all state objects from database"""
 
 
-
-
-
 import sys
-
 from model_state import Base, State
-
 from sqlalchemy.orm import sessionmaker
-
 from sqlalchemy import create_engine
 
 
-
-
-
 if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+                        sys.argv[1], sys.argv[2], sys.argv[3]),
+                        pool_pre_ping=True
+                    )
+    Session = sessionmaker(bind=engine)
+    Base.metadata.create_all(engine)
 
-        engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+    session = Session()
 
-                                    sys.argv[1], sys.argv[2], sys.argv[3]),
+    states = session.query(State).order_by(State.id).all()
 
-                                                            pool_pre_ping=True
+    for state in states:
+        print(f"{state.id}: {state.name}")
 
-                                                                                )
-
-            Session = sessionmaker(bind=engine)
-
-                Base.metadata.create_all(engine)
-
-
-
-                    session = Session()
-
-
-
-                        states = session.query(State).order_by(State.id).all()
-
-
-
-                            for state in states:
-
-                                        print(f"{state.id}: {state.name}")
-
-
-
-                                            session.close()
+    session.close()
