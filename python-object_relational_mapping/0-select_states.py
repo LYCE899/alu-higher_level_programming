14046061204a45dffe7b806ea749bb28d2a607c2
Coulibaly Amani <109763914+LYCE899@users.noreply.github.com>
2022-12-15 12:58:02 +0200

    Update 0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -8,6 +8,18 @@ USE hbtn_0e_0_usa;
 CREATE TABLE IF NOT EXISTS states ( 
     id INT NOT NULL AUTO_INCREMENT, 
     name VARCHAR(256) NOT NULL,
-    PRIMARY KEY (id)
-);
+    PRIMARY KEY (id)  
+);    
+  
+    if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
+                           user=argv[1], passwd=argv[2], db=argv[3])
+
 INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas"), ("New York"), ("Nevada");
+cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
