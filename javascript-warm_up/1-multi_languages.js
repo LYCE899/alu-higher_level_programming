6beae6cf7c44bfce275274de65dd556cfd2859c7
LYCE899 <a.coulibaly@alustudent.com>
2023-02-06 13:12:36 -0800

    JSamazing

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-const myVar = "C is fun
-Python is cool
-JavaScript is amazing";
-console.log(myVar);
+console.log("C is fun");
+console.log("Python is cool");
+console.log("JavaScript is amazing");
