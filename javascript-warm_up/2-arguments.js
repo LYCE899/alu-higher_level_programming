2d849e5f8b3b0bf02435b7eab68e96ebfcc15661
LYCE899 <a.coulibaly@alustudent.com>
2023-02-07 08:53:44 -0800

    JS amazing

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,7 +1,7 @@
 #!usr/bin/node
 if ((process.argv).length = 3)
   console.log('No argument'); 
-   if ((process.argv).length = 6) 
+if ((process.argv).length = 6) 
   console.log('Argument found'); 
-  else (process.argv) 
+else (process.argv) 
   console.log('Arguments found');
