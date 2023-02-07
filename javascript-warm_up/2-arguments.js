6e8543597b2ed0ab7581af4aa495886629cfd5bd
Coulibaly Amani <109763914+LYCE899@users.noreply.github.com>
2023-02-07 18:37:14 +0200

    Update 2-arguments.js

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,10 +1,8 @@
 #!usr/bin/node
 if ((process.argv).length = 3)
-  console.log('No argument')
-  else if ((process.argv).length = 6) {
-  console.log('Argument found');
-}
-  else if (process.argv)Width == 3
-{
+  console.log('No argument'); {
+  else if ((process.argv).length = 6) 
+  console.log('Argument found'); } 
+{ else  (process.argv)Width == 3
   console.log('Arguments found');
 }
