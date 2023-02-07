91c0d6b7b21274ef92dcc0f817ccdb0234ac4159
Coulibaly Amani <109763914+LYCE899@users.noreply.github.com>
2023-02-08 00:57:45 +0200

    Update 7-multi_c.js

---
+++ javascript-warm_up/7-multi_c.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const nm = parseInt(process.argv[2]);
+if (nm) {
+  for (let i = 0; i < process.argv[2]; i++) {
+    console.log('C is fun');
+  }
+} else {
+  console.log('Missing number of occurences');
+}
