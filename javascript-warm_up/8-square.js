c345e1e98bc0610e5a56fc63cad381a6ec69fe30
Coulibaly Amani <109763914+LYCE899@users.noreply.github.com>
2023-02-08 20:53:29 +0200

    Update 8-square.js

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,16 +1,9 @@
 #!/usr/bin/node
-function square () {
-  const n = parseInt(process.argv[2]);
-  if (n) {
-    for (let i = 0; i < n; i++) {
-      if (n > 0) {
-        console.log('X'.repeat(n));
-      } else {
-        break;
-      }
-    }
-  } else {
-    console.log('Missing size');
-  }
+
+const arg = Number(process.argv[2]);
+if (isNaN(arg)) {
+  console.log('Missing size');
 }
-square.call();
+for (let i = 0; i < arg; i++) {
+  console.log('X'.repeat(arg));
+}
