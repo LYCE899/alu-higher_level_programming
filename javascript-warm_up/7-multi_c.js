a61059a7a6d3101c6a1e42a06a6a7e6af7db135d
LYCE899 <a.coulibaly@alustudent.com>
2023-02-08 10:12:25 -0800

    ff

--- javascript-warm_up/7-multi_c.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-const nm = parseInt(process.argv[2]);
-if (nm) {
-  for (let i = 0; i < process.argv[2]; i++) {
-    console.log('C is fun');
-  }
-} else {
-  console.log('Missing number of occurences');
-}
