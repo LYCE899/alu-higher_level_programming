12b3e458f323531d0feac56ec02d4eea3b53904e
Coulibaly Amani <109763914+LYCE899@users.noreply.github.com>
2023-02-08 21:38:55 +0200

    Update 10-factorial.js

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,8 +1,14 @@
 #!/usr/bin/node
+const arg = Number(process.argv[2]);
+if (isNaN(arg)) {
+  console.log(1);
+} else {
+  console.log(factorial(arg));
+}
+
 function factorial (num) {
-  if (isNaN(num) || num <= 1) {
+  if (num === 0) {
     return 1;
   }
   return num * factorial(num - 1);
 }
-console.log(factorial(parseInt(process.argv[2])));
