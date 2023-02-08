cf4d5947381cdf5ff1be6b8e056869cf6880de12
Coulibaly Amani <109763914+LYCE899@users.noreply.github.com>
2023-02-08 22:18:15 +0200

    Update 6-square.js

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+class Square extends require('./5-square.js') {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Square;
