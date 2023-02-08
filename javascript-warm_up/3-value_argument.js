55bcfb1997a3684bb8373df1db8887cd1a9de908
Coulibaly Amani <109763914+LYCE899@users.noreply.github.com>
2023-02-08 13:46:09 +0200

    Update 3-value_argument.js

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
-const args = process.argv.slice(2);
-if (args[0] == null) {
-  console.log('No argument');
+if (process.argv[2]) {
+  console.log(process.argv[2]);
 } else {
-  console.log(args[0]);
+  console.log('No argument');
 }
