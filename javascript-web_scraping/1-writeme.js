b2ca175d08aaceed374a14614bb2f2a265de5c6d
Coulibaly Amani <109763914+LYCE899@users.noreply.github.com>
2023-02-09 16:59:47 +0200

    Update 1-writeme.js

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,1 +1,11 @@
-wiwi
+#!/usr/bin/node
+const fs = require('fs');
+
+const filePath = process.argv[2];
+const stringToWrite = process.argv[3];
+
+fs.writeFile(filePath, stringToWrite, 'utf-8', (error) => {
+  if (error) {
+    console.error(error);
+  }
+});
