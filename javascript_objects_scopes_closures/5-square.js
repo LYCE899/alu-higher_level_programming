761d0d640226f38cec36135a8e1e32fe343bc409
Coulibaly Amani <109763914+LYCE899@users.noreply.github.com>
2023-02-09 14:04:10 +0200

    Update 5-square.js

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,1 +1,8 @@
-sysy
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+}
+module.exports = Square;
