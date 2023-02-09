02519abda1c92c68d95db03356350c1a62109f17
Coulibaly Amani <109763914+LYCE899@users.noreply.github.com>
2023-02-09 17:43:21 +0200

    Update 6-completed_tasks.js

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,33 @@
-wiwi
+#!/usr/bin/node
+// Writing a script that computes the number of tasks completed by user id.
+const request = require('request');
+
+function getCompletedTasks (data, userId) {
+  let count = 0;
+  data
+    .filter((element) => element.userId === userId)
+    .forEach((task) => {
+      if (task.completed) {
+        count++;
+      }
+    });
+  return count;
+}
+
+const url = process.argv[2];
+
+const results = {};
+request.get(url, (error, response) => {
+  if (error) {
+    throw error;
+  }
+  const data = JSON.parse(response.body);
+  data.forEach((element) => {
+    if (!(element.userId in results)) {
+      if (getCompletedTasks(data, element.userId) > 0) {
+        results[element.userId] = getCompletedTasks(data, element.userId);
+      }
+    }
+  });
+  console.log(results);
+});
