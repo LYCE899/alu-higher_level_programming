0dd7198cdf8cf2ea5db69ad892ccb4eec9dfca54
Coulibaly Amani <109763914+LYCE899@users.noreply.github.com>
2023-02-09 17:04:10 +0200

    Update 0-readme.js

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,1 +1,12 @@
-wiwi
+#!/usr/bin/node
+const fs = require('fs');
+
+const filePath = process.argv[2];
+
+fs.readFile(filePath, 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});
