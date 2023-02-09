7d12ae1f7c227d5771a9a73728aa40ee8a2c2f87
Coulibaly Amani <109763914+LYCE899@users.noreply.github.com>
2023-02-09 17:24:02 +0200

    Update 3-starwars_title.js

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,12 @@
-wiwi
+#!/usr/bin/node
+const request = require('request');
+
+const movieID = process.argv[2];
+
+request(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`, function (error, response, body) {
+  if (!error && response.statusCode === 200) {
+    console.log(JSON.parse(body).title);
+  } else {
+    console.log('An error has occurred');
+  }
+});
