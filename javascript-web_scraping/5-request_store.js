e6c077823942a9ad3f728db3bd765e4b231627d6
Coulibaly Amani <109763914+LYCE899@users.noreply.github.com>
2023-02-09 17:37:10 +0200

    Update 5-request_store.js

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,1 +1,16 @@
-wiwi
+#!/usr/bin/node
+// Writing a script that gets the contents of a webpage and stores it in a file
+const request = require('request');
+const fs = require('fs');
+const args = process.argv.slice(2);
+request(args[0], (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    fs.writeFile(args[1], body, (error) => {
+      if (error) {
+        console.log(error);
+      }
+    });
+  }
+});
